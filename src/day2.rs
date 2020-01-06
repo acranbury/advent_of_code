02fc4c63736876b0f3c9e1ae8f2c6bc66a57a681
Alan Cranbury <alancranbury@gmail.com>
2020-01-05 23:23:17 -0500

    Adds preliminary day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,16 @@
-pub run(contents: &str){
-	
+pub fn run(contents: &str){
+}
+
+fn computer(program: &Vec<i32>) {
+
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn one_plus_one() {
+		// let program = Vec::new();
+	}
 }
