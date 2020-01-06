dbe826a33da6678853a8d5e6ec3f61b369bc2ab1
Alan Cranbury <alancranbury@gmail.com>
2020-01-05 23:22:30 -0500

    Adds unit tests for day1

--- src/day1.rs
+++ src/day1.rs
@@ -9,11 +9,32 @@ pub fn run(contents: &str) {
 	println!("Total fuel: {}", fuel);
 }
 
-pub fn get_fuel_cost(mass: i32) -> i32 {
-	let fuel =(mass / 3) - 2;
+fn get_fuel_cost(mass: i32) -> i32 {
+	let fuel = fuel_calc(mass);
 	if fuel <= 0 {
 		0
 	} else {
 		fuel + get_fuel_cost(fuel)
+	}
+}
+
+fn fuel_calc(mass: i32) -> i32 {
+	(mass / 3) - 2
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn simple_fuel_calcs() {
+		assert_eq!(fuel_calc(1969), 654);
+		assert_eq!(fuel_calc(100756), 33583);
+	}
+
+	#[test]
+	fn complex_fuel_calcs() {
+		assert_eq!(get_fuel_cost(1969), 966);
+		assert_eq!(get_fuel_cost(100756), 50346);
 	}
 }
