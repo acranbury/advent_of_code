31b9285c67b9b799c55fb9d1ed914824181c4c0f
Cranbury, Alan <Alan.Cranbury@christiedigital.com>
2020-01-07 08:44:23 -0500

    Adds tests from problem to day 2

--- src/day2.rs
+++ src/day2.rs
@@ -11,6 +11,41 @@ mod tests {
 
 	#[test]
 	fn one_plus_one() {
-		// let program = Vec::new();
+		let mut actual = vec![1,0,0,0,99];
+		let expected = vec![2,0,0,0,99];
+
+		intcode_computer(&mut actual);
+
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn three_times_two() {
+		let mut actual = vec![2,3,0,3,99];
+		let expected = vec![2,3,0,6,99];
+
+		intcode_computer(&mut actual);
+
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn ninety_nine_squared() {
+		let mut actual = vec![2,4,4,5,99,0];
+		let expected = vec![2,4,4,5,99,9801];
+
+		intcode_computer(&mut actual);
+
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn modify_code_before_executing() {
+		let mut actual = vec![1,1,1,4,99,5,6,0,99];
+		let expected = vec![30,1,1,4,2,5,6,0,99];
+
+		intcode_computer(&mut actual);
+
+		assert_eq!(actual, expected);
 	}
 }
