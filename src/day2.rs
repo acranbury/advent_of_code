4e1f06ce751ebde8fd4f2b444c6b8c8fc98fdf8a
Cranbury, Alan <Alan.Cranbury@christiedigital.com>
2020-01-09 16:45:00 -0500

    Fleshes out day 2 intcode computer

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,9 @@
+use std::process;
+
 pub fn run(contents: &str){
-	let mut intcode: Vec<i32> = contents.split(',').map(|x| x.parse().unwrap()).collect();
+	let mut intcode: Vec<i32> = contents.split(',')
+										.map(|x| x.parse().unwrap())
+										.collect();
 
 	intcode_computer(&mut intcode);
 
@@ -8,8 +12,38 @@ pub fn run(contents: &str){
 	}
 }
 
-fn computer(program: &Vec<i32>) {
+const OPCODE_ADD: i32 = 1;
+const OPCODE_MUL: i32 = 2;
 
+fn intcode_computer(program: &mut Vec<i32>) {
+	let mut pc: usize = 0;
+	let mut opcode = program[pc];
+
+	while opcode != 99 {
+		match opcode {
+			OPCODE_ADD => {
+				let index: usize = program[pc + 3] as usize;
+				program[index] = program[index + 1] + program[index + 2]
+			},
+			OPCODE_MUL => {
+				let index: usize = program[pc + 3] as usize;
+				program[index] = program[index + 1] * program[index + 2]
+			},
+			_ => {
+				println!("Unknown Opcode! {}", opcode);
+				process::exit(1);
+			},
+		};
+
+		pc += 4;
+
+		if pc < program.len() {
+			opcode = program[pc];
+		} else {
+			println!("Program ended unexpectedly! pc: {}", pc);
+			process::exit(1);
+		}
+	}
 }
 
 #[cfg(test)]
@@ -19,7 +53,7 @@ mod tests {
 	#[test]
 	fn one_plus_one() {
 		let mut actual = vec![1,0,0,0,99];
-		let expected = vec![2,0,0,0,99];
+		let expected   = vec![2,0,0,0,99];
 
 		intcode_computer(&mut actual);
 
@@ -29,7 +63,7 @@ mod tests {
 	#[test]
 	fn three_times_two() {
 		let mut actual = vec![2,3,0,3,99];
-		let expected = vec![2,3,0,6,99];
+		let expected   = vec![2,3,0,6,99];
 
 		intcode_computer(&mut actual);
 
@@ -39,7 +73,7 @@ mod tests {
 	#[test]
 	fn ninety_nine_squared() {
 		let mut actual = vec![2,4,4,5,99,0];
-		let expected = vec![2,4,4,5,99,9801];
+		let expected   = vec![2,4,4,5,99,9801];
 
 		intcode_computer(&mut actual);
 
@@ -49,7 +83,7 @@ mod tests {
 	#[test]
 	fn modify_code_before_executing() {
 		let mut actual = vec![1,1,1,4,99,5,6,0,99];
-		let expected = vec![30,1,1,4,2,5,6,0,99];
+		let expected   = vec![30,1,1,4,2,5,6,0,99];
 
 		intcode_computer(&mut actual);
 
